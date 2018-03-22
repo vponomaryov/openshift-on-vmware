423895dbc9ef0ddb6de28c562f5c626c1bf2e901
Valeriy Ponomaryov <kiparis.kh@gmail.com>
2018-03-22 23:22:43 +0530

    [VMWare] Add possibility to choose openshift-ansible lib version
    Now, by default, for each major version of OCP we will use
    latest version of 'openshift-ansible' lib.
    To use custom version just define following env var:
    
    $ export OPENSHIFT_ANSIBLE_GIT_TAG=openshift-ansible-3.X.Y

--- reference-architecture/vmware-ansible/scripts/install_openshift_ansible.sh
+++ reference-architecture/vmware-ansible/scripts/install_openshift_ansible.sh
@@ -8,12 +8,23 @@
 
 OPENSHIFT_ANSIBLE_GIT_URL='git://github.com/openshift/openshift-ansible.git'
 TARGET_DIR=$1/usr/share/ansible/openshift-ansible
+TAG=$2
+
+if [ -z "$TAG" ]; then
+    # NOTE(vponomar): get latest tag by 3.X branch
+    TAG=$(git ls-remote --tags $OPENSHIFT_ANSIBLE_GIT_URL \
+        "refs/tags/openshift-ansible-$(echo $1 | grep -oE '[^tox\/ocp]+$').*" \
+        | grep -v "\{\}" | sort -t / -k 3 -V | tail -n 1 | awk '{print $2}' )
+    echo "Custom Git tag hasn't been specified, using latest Git tag '$TAG'"
+else
+    echo "Using custom Git tag '$TAG'"
+fi
 
 if [[ ! -d $TARGET_DIR ]]; then
     mkdir -p $TARGET_DIR
-    git clone $OPENSHIFT_ANSIBLE_GIT_URL --single-branch --branch $2 $TARGET_DIR
+    git clone $OPENSHIFT_ANSIBLE_GIT_URL --single-branch --branch $TAG $TARGET_DIR
 else
     cd $TARGET_DIR
     git fetch -t --all
-    git reset --hard $2
+    git reset --hard $TAG
 fi
